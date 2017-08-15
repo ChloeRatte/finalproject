df57de028222ad33f5042214afb8c8306b9cd981
kyliemenaGWC <daisymena2000@gmail.com>
2017-08-15 13:06:05 -0400

    More HTML

--- fs.js
+++ fs.js
@@ -19,8 +19,9 @@ function initMap() {
       }
 
 
+//explain to Kylie LAter
 
-function myFunction(){
+function myFunction1(){
 	
 }
 function myFunction2(){
@@ -40,4 +41,25 @@ function myFunction3(){
 		document.getElementById("demo").innerHTML = " ";
 	}
 	tog3 = !tog3;
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
