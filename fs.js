042d14dd8477184e4f42a9c6ec911edf35bc32c4
Grace Gonzalez <Grace@Graces-MacBook-Pro.local>
2017-08-21 14:00:43 -0400

    Random recipe for else statement

--- fs.js
+++ fs.js
@@ -87,19 +87,23 @@ function getData() {
         }
 
         else{
+            var num = 0;
+            var x = Math.floor(Math.random()*yieldOptions.length);
+            num = x; 
+
             var recipeDiv = document.getElementById("recipeName");
-            var recipeName = document.createTextNode(dietOptions[0].label);
+            var recipeName = document.createTextNode(dietOptions[num].label);
             var recipeImage = document.createElement("img")
-            recipeImage.setAttribute("src", dietOptions[0].image);
+            recipeImage.setAttribute("src", dietOptions[num].image);
             document.getElementById("recipeImage").appendChild(recipeImage);
             recipeDiv.appendChild(recipeName);
 
 //================================          
-            for (var i=0; i<dietOptions[0].ingredients.length; i++) {
+            for (var i=0; i<dietOptions[num].ingredients.length; i++) {
                 // console.log(i);
 
                 var theDiv = document.getElementById("yourRecipe"); 
-                var content = document.createTextNode(dietOptions[0].ingredients[i].text);
+                var content = document.createTextNode(dietOptions[num].ingredients[i].text);
                 linebreak = document.createElement("br");
                 theDiv.appendChild(linebreak);
                 theDiv.appendChild(content);
@@ -108,13 +112,13 @@ function getData() {
 
                 // document.getElementById("yourRecipe").innerHTML = dietOptions[0].ingredients[i].text;
               }
-            for (var j=0; j<dietOptions[0].digest.length; j++) {
-              var nutriAmount = dietOptions[0].digest[j].total.toFixed(0);
+            for (var j=0; j<dietOptions[num].digest.length; j++) {
+              var nutriAmount = dietOptions[num].digest[j].total.toFixed(0);
 
               var nutritionDiv = document.getElementById("yourNutrition");
-              var nutritionContent = document.createTextNode(dietOptions[0].digest[j].label);
+              var nutritionContent = document.createTextNode(dietOptions[num].digest[j].label);
               var nutritionAmount = document.createTextNode(nutriAmount);
-              var nutritionUnit = document.createTextNode(dietOptions[0].digest[j].unit);
+              var nutritionUnit = document.createTextNode(dietOptions[num].digest[j].unit);
               linebreak = document.createElement("br");
               nutritionDiv.appendChild(linebreak);
               nutritionDiv.appendChild(nutritionContent);
